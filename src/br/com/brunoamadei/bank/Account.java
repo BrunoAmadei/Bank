e8ea0b99e97d6628d1e93d0fc46411048c71e77b
BrunoAmadei <brunoamadei98@gmail.com>
2023-11-27 13:34:34 -0300

    constructor Conta + definindo tamanho de name

--- src/br/com/brunoamadei/bank/Account.java
+++ src/br/com/brunoamadei/bank/Account.java
@@ -1,7 +1,24 @@
 package br.com.brunoamadei.bank;
 
 public class Account {
-    String ag;
-    String cc;
-    String name;
+    private String ag;
+    private String cc;
+    private String name;
+
+    private static final int MAX_LENGTH = 12;
+
+    public Account(String ag, String cc, String name) {
+        this.ag = ag;
+        this.cc = cc;
+        setName(name);
+    }
+
+    public void setName(String name) {
+        if (name.length() > MAX_LENGTH) {
+            this.name = name.substring(0, MAX_LENGTH);
+        } else {
+            this.name = name;
+        }
+        System.out.println(this.name);
+    }
 }
--- src/br/com/brunoamadei/bank/App.java
+++ src/br/com/brunoamadei/bank/App.java
@@ -2,9 +2,10 @@ package br.com.brunoamadei.bank;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-
+        
         // criar conta (agencia, conta, nome)
+        Account account = new Account("0001", "1234", "Bruno Amadei");
+
         // limitar nome
         // sacar valores (não pode sacar mais do que tem)
         // depositar
