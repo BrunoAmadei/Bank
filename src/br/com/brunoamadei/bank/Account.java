73e5d23cd4489bf863c4fc6c8fe55ab63a65cd36
BrunoAmadei <brunoamadei98@gmail.com>
2023-11-27 10:23:28 -0300

    update app (estrutura de pacotes)

---
+++ src/br/com/brunoamadei/bank/Account.java
@@ -0,0 +1,7 @@
+package br.com.brunoamadei.bank;
+
+public class Account {
+    String ag;
+    String cc;
+    String name;
+}
---
+++ src/br/com/brunoamadei/bank/App.java
@@ -0,0 +1,13 @@
+package br.com.brunoamadei.bank;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        // criar conta (agencia, conta, nome)
+        // limitar nome
+        // sacar valores (não pode sacar mais do que tem)
+        // depositar
+        // informar operações para usuario
+    }
+}
