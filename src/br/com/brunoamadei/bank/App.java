2d38d8cdce1717582262e21d38cadfb91485f05a
BrunoAmadei <brunoamadei98@gmail.com>
2023-11-29 13:38:11 -0300

    classe Scanner incluida

--- src/br/com/brunoamadei/bank/App.java
+++ src/br/com/brunoamadei/bank/App.java
@@ -1,26 +1,29 @@
 package br.com.brunoamadei.bank;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-
         // criar conta (agencia, conta, nome)
         Account account = new Account("0001", "1234", "Madruguinha Oliver");
 
-        
-        // depositar
-        account.deposit(150);
-        account.deposit(50);
-        account.deposit(100);
-        
-        // sacar valores (não pode sacar mais do que tem)
-        if (!account.withDraw(150.00)) {
-            System.out.println("Não há saldo suficiente para sacar");
-        }
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Qual operação deseja realizar? D-Depósito | S-Saque | E-Exit(sair)");
+            String op = scanner.nextLine();
 
-        if (!account.withDraw(85.45)) {
-            System.out.println("Não há saldo suficiente para sacar");
+            if (op.equals("D")) {
+                System.out.println("Qual valor deseja depositar?");
+                double value = scanner.nextDouble();
+                account.deposit(value);
+            } else if (op.equals("S")) {
+                System.out.println("Qual valor deseja Sacar?");
+                double value = scanner.nextDouble();
+                account.withDraw(value);
+            } else if (op.equals("E")) {
+                System.out.println("Saindo do sistema...");
+                break;
+            }
         }
-        // informar usuario sobre a conta
-        System.out.println(account);
     }
 }
